e5220f5aa136b91b7be0ca9c2148140a4846efb0
Imghost <imghostcode@gmail.com>
2023-02-08 08:00:47 -0500

    use logger

--- server/server.js
+++ server/server.js
@@ -2,25 +2,33 @@ require("dotenv").config();
 const app = require("./src/app");
 const config = require("./src/v1/config/index");
 const mongoose = require("./src/v1/databases/init.mongodb");
+const logger = require('./src/v1/utils/logger')
 
 const PORT = config.app.port;
 const MONGO_URI = config.db.uri;
+let server
 async function startServer() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected mongoose success!...");
-    const server = app.listen(PORT, () => {
+    await mongoose.connect(MONGO_URI);
+    logger.info("Connected mongoose success!...")
+    server = app.listen(PORT, () => {
       console.log(`Server start with port ${PORT}`);
     });
 
-    // process.on("SIGINT", () => {
-    //   server.close(() => console.log(`Exits server express`));
-    //   process.exit(0);
-    // });
+
   } catch (error) {
-    console.log("Cannot connect to database! ", error);
-    process.exit();
+    logger.error(`Cannot connect to database! ${error.message}`)
+    process.exit(0);
   }
 }
+
+process.on("SIGINT", async () => {
+  if (server) {
+    await server.close(() => console.log(`Exits server express`));
+    await mongoose.connection.close()
+    process.exit(0);
+  }
+
+});
 
 startServer();
--- server/src/v1/databases/init.mongodb.js
+++ server/src/v1/databases/init.mongodb.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const logger = require("../utils/logger");
 
 // all executed methods log output to console
 mongoose.set("debug", true);
@@ -8,5 +9,9 @@ mongoose.set("debug", { color: false });
 
 // get mongodb-shell friendly output (ISODate)
 mongoose.set("debug", { shell: true });
+
+mongoose.connection.on('disconnected', () => {
+    logger.info('Disconnected to mongodb..')
+})
 
 module.exports = mongoose;
